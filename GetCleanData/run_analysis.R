aaf6f8abbf6c0422a3670fd203fc0851e460bd65
ehemdal <ehemdal@townisp.com>
2014-09-14 12:23:09 -0400

    Committing start of real work on project.
    Saving quiz 1 files for record.

--- GetCleanData/run_analysis.R
+++ GetCleanData/run_analysis.R
@@ -9,8 +9,11 @@
 # You should create one R script called run_analysis.R that does the following. 
 
 # Obtain the data, unless we already have it
-setwd("C:\\DataScience")
-if (!file.exists("data.zip")) {
+# Grab our current WD, and save it; then go to the chosen work directory so 
+# that we'll leave the WD where it started
+oldWD <- getwd()
+setwd("C:/git/datasciencecoursera/GetCleanData")
+if (!file.exists("rawdata.zip")) {
   setInternet2(TRUE)
   sourceurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   destfile <-"./rawdata.zip"
@@ -18,9 +21,15 @@ if (!file.exists("data.zip")) {
 } else {
   print("File already exists in directory, not downloading.")
 }
+
+# Unzip the rawdata.zip file, if necessary
+print("Unzipping the raw data.")
+unzip(destfile)
 # Merges the training and the test sets to create one data set.
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names. 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
+# Cleanup
+setwd(oldWD)
