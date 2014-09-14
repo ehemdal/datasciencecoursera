09aac42a115b387d7aaad4adaf03ce70c51fb6a7
ehemdal <ehemdal@townisp.com>
2014-09-14 16:28:15 -0400

    I think I have the completed dataset cleaned up now; saving.

--- GetCleanData/run_analysis.R
+++ GetCleanData/run_analysis.R
@@ -23,13 +23,50 @@ if (!file.exists("rawdata.zip")) {
 }
 
 # Unzip the rawdata.zip file, if necessary
+if (!file.exists("UCI HAR Dataset")) {
 print("Unzipping the raw data.")
 unzip(destfile)
+} else {
+  print ("Dataset already unzipped.")
+}
 # Merges the training and the test sets to create one data set.
+  # 1) Get the activity labels and features
+    setwd("UCI HAR Dataset")
+    activities <- read.table("activity_labels.txt")
+    activities <- as.vector(activities$V2)
+    features <- read.table("features.txt")
+    features <- as.vector(features$V2)
+  # 2) Read the test dataset into td
+    setwd("test")
+    # td is a data.frame holding the test data with activity numbers in first col.
+    data <- read.table("X_test.txt")
+    acts <- read.table("y_test.txt")
+    testd <- cbind(acts,data)
+
+  # 3) Read the training dataset into traind; remove data and acts when done
+    setwd("../train")
+    data <- read.table("X_train.txt")
+    acts <- read.table ("y_train.txt")
+    traind <- cbind(acts,data)
+    rm(acts,data)
+
+  # 4) Combine the two datasets 
+  #    Add column names
+  #    Change first column to descriptive activity names
+    data <- rbind(testd, traind)
+    rm(testd, traind)
+    vars <- c("Activity", as.vector(features))
+    colnames(data) <- vars
+    for(i in 1:nrow(data)){
+      index <- as.integer(data[i,1])
+      data[i,1] <- activities[index]
+    }
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names. 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-# Cleanup
-setwd(oldWD)
+# Cleanup - run this after the rest is working OK
+#setwd(oldWD)
+return()
+
