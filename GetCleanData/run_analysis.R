4dc1e3e0e60f09c76f271c458aecfe17ced41353
ehemdal <ehemdal@townisp.com>
2014-09-16 20:14:01 -0400

    Code finally produces the "wide tidy" data, I think.
    Still need to add the codebook.

--- GetCleanData/run_analysis.R
+++ GetCleanData/run_analysis.R
@@ -31,6 +31,7 @@ unzip(destfile)
 }
 # Merges the training and the test sets to create one data set.
   # 1) Get the activity labels and features
+    print("Getting the training dataset.")
     setwd("UCI HAR Dataset")
     activities <- read.table("activity_labels.txt")
     activities <- as.vector(activities$V2)
@@ -38,6 +39,7 @@ unzip(destfile)
     features <- as.vector(features$V2)
 
   # 2) Read the test dataset into data
+    print("Get the test dataset.")
     setwd("test")
     # testd is a data.frame holding the test data with activity numbers in first col.
     data <- read.table("X_test.txt")
@@ -51,7 +53,7 @@ unzip(destfile)
     acts <- read.table ("y_train.txt")
     subjects <- read.table("subject_train.txt")
     traind <- cbind(subjects,acts,data)
-    rm(subjects,acts,data)
+    #rm(subjects,acts,data)
 
   # 4) Combine the two datasets 
     data <- rbind(testd, traind)
@@ -67,22 +69,50 @@ unzip(destfile)
   data <- data[,keeps]
 
   #    Change first column to descriptive activity names
-
-
+    print ("Adding descriptive names.")
     for(i in 1:nrow(data)){
       index <- as.integer(data[i,2])
       data[i,2] <- activities[index]
     }
 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject.   
-  data2 <- tbl_df(data)
-  rm(data) # save space
-  data3 <- group_by(data2, Subject_ID, Activity)
+  print("Creating data table frame from data frame.")
+  data <- tbl_df(data)
+  # Filter out each group, drop the first two columns (which we won't need once
+  # we filter) and then use colMeans to get the averages.  Build this into
+  # a new data frame called result
 
-# next steps: make sure group_by is OK and figure out summarize() from dplyr
-  
-# Go pull out the right columns
+  print("Filtering and averaging.")
+  result <- data.frame(row.names = colnames(data))
+  for (subj in 1:30) {
+    for (act in activities) {
+      tempdf <- filter(data, Subject_ID == subj, Activity == act)
+      tempdf <- tempdf[-c(1,2)]
+      result <- rbind(result, colMeans(tempdf))
+    }
+  }
+  # Add descriptive naes
+  i <- 1
+  acts <- ""
+  subs <- ""
+  for (subj in 1:30) { 
+    for (act in activities) {
+      # build vectors that we can cbind onto result
+      acts <- c(acts, act)
+      subs <- c(subs, subj)
+    }
+  }
+  result <- cbind(acts[2:length(acts)], result)
+  result <- cbind(subs[2:length(subs)], result)
+  colnames(result) <- names(data)
+  rownames(result) <- NULL
+
+# Write the final dataset
+print ("writing final dataset.")
+setwd("C:/git/datasciencecoursera/GetCleanData")
+write.table(result, file = "tidy_avg.txt", row.names = FALSE)
+
 # Cleanup - run this after the rest is working OK
 setwd(oldWD)
-return()
+
 
